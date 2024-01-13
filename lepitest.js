67cf11bb26e0f385560bebed425947482e2a97d5
Georg Hammerschmid <8532340+lepitaxa@users.noreply.github.com>
2024-01-13 19:07:50 +0100

    Added support for basionyms + basionym authors
    - Introduced data point classes b and n
    - b being the basionym genus
    - n being the basionym author + year
    - Added some exemplary b+n data point pairs, e.g. Papilio machaon LINNAEUS, 1758
    - Updated Lepitest to check and convert everything correctly
    - Readme update not done yet ...

--- lepitest.js
+++ lepitest.js
@@ -28,10 +28,10 @@ var sf = 'The suffix of this taxon type must be "'
 
 
 // Special characters and capitalization
-if (!f.matches('.xp') && /( |\t)$/.test(d) || /(  |\.\.|--| † †| agg. agg.)/g.test(d) || f.matches('p:not(.l,.xp)') && /"/g.test(d)) {i++; log += '[#' + i + s1 + 'Check for excess spaces, tabs, special characters or suffixes.\n'};
-if (f.matches('[class^="x"]:not(.xp),.f,.y,.t,.j,.h,.g,.i,.o,.sh') && /[^A-z]/g.test(d)) {i++; log += '[#' + i + s1 + 'Allowed characters are A-Z and a-z only. Check for invalid spaces, tabs, line-breaks and special characters.\n'};
+if (!f.matches('.xp') && /( |\t)$/.test(d) || /(  |\.\.|--| † †| agg. agg.)/g.test(d) || f.matches('.bs,p:not(.b,.l,.xp)') && /"/g.test(d)) {i++; log += '[#' + i + s1 + 'Check for excess spaces, tabs, special characters or suffixes.\n'};
+if (f.matches('[class^="x"]:not(.xp),.f,.y,.t,.j,.h,.g,.i,.o,.bs,.sh') && /[^A-z]/g.test(d)) {i++; log += '[#' + i + s1 + 'Allowed characters are A-Z and a-z only. Check for invalid spaces, tabs, line-breaks and special characters.\n'};
 if (f.matches('.a,.ae,.c,.m,.u,.ue,.w,.v,.v2,.s,.s2,.s3,.s4') && /[^A-z-.†× ]/g.test(d)) {i++; log += '[#' + i + s1 + 'Allowed characters are A-Z, a-z and single spaces or hyphens only. Check for invalid tabs, line-breaks and special characters.\n'};
-if (f.matches('p:not(.l,.p,.xp)') && /[^A-ZÄÖÕÜ]/.test(d.substring(0,1)) || f.matches('.xp') && /[^A-Z]/.test(d.substring(1,2)) || f.matches('p:not(.xp,.r,.r2,.l,.p,.p2,.e,.e2,.d,.d2)') && /[^a-z -.†×;]/.test(d.substring(1,d.length)) || f.matches('.xp') && /[^a-z]/.test(d.substring(2,d.length-2))) {i++; log += '[#' + i + s1 + 'Check for correct placement of uppercase and lowercase letters.\n'};
+if (f.matches('.bs,p:not(.b,.n,.l,.p,.xp)') && /[^A-ZÄÖÕÜ]/.test(d.substring(0,1)) || f.matches('.xp') && /[^A-Z]/.test(d.substring(1,2)) || f.matches('.bs,p:not(.xp,.b,.n,.r,.r2,.l,.p,.p2,.e,.e2,.d,.d2)') && /[^a-z -.†×;]/.test(d.substring(1,d.length)) || f.matches('.xp') && /[^a-z]/.test(d.substring(2,d.length-2))) {i++; log += '[#' + i + s1 + 'Check for correct placement of uppercase and lowercase letters.\n'};
 if (f.matches('.d,.d2,.e,.e2') && (/ [a-z]/g.test(d) && !(/(and|of|du|und|von)/.test(d)) || /( And | Of | Du | Und | Von )/.test(d))) {i++; log += '[#' + i + s1 + 'Check for correct placement of uppercase and lowercase letters.\n'};
 
 // Common typos, spelling and grammar mistakes
@@ -70,7 +70,8 @@ if (f.matches('.j') && !(/ina$/.test(d))) {i++; log += '[#' + i + s1 + sf + 'ina
 if (f.matches('.h') && !(/iti$/.test(d))) {i++; log += '[#' + i + s1 + sf + 'iti".\n'};
 if (f.matches('p:not(.c)') && (/ agg\.$/.test(d))) {i++; log += '[#' + i + s1 + 'Use the AGG datatype to add the "agg." suffix to a species complex.\n'};
 
-// REF errors
+// BAS and REF errors
+if (f.matches('.n') && (!(/^\D+, (1|2)(7|8|9|0)\d\d$/.test(d)) || d.substring(d.length-4,d.length) < 1758)) {i++; log += '[#' + i + s1 + "Make sure the BAS_AUT ends with a valid date in , YYYY format, starting at 1758.\n"};
 if (f.matches('.r,.r2') && !(/, 20\d\d$/.test(d))) {i++; log += '[#' + i + s1 + "Make sure the REF ends with a valid date in 'YY format.\n"};
 if (f.matches('.r') && (d.match(/,/g) || []).length > 1) {i++; log += '[#' + i + s1 + "Only list the first author when using the r data type.\n"};
 if (f.matches('.r2') && (d.match(/,/g) || []).length > 2) {i++; log += '[#' + i + s1 + "Don't list more than 2 authors when using the r2 data type.\n"};
@@ -82,17 +83,19 @@ if (f.matches('.x9') && !(f.matches('p:first-of-type'))) {i += 1; log += '[#' + 
 if (f.matches('.f,.i,.o,.a,.ae,.c,.m,.u,.ue,.w,.v,.v2') && f.matches('p:first-of-type') || f.matches('.m,.u,.ue') && f.matches('p:nth-of-type(2)')) {i++; log += '[#' + i + s2 + 'Must be located within a valid parent taxon.\n'};
 if (f.matches('.e2,.d2') && f.matches('p.g~p')) {i++; log += '[#' + i + s4 + 'This type 2 common name must be located below a SUPERFAM, FAM, SUBFAM, TRI, SUBTRI or INFRATRI taxon.\n'};
 if (f.matches('.sh') && f.matches('p.g~p')) {i++; log += '[#' + i + s4 + 'This synonym must be located below a ORD, PARAPHYLUM, SUPERFAM, FAM, SUBFAM, TRI, SUBTRI or INFRATRI taxon.\n'};
-if (f.matches('.e,.d,.s,.s2,.s3,.s4') && !(f.matches(':is(p.a,p.ae,p.c,.w)~p'))) {i++; log += '[#' + i + s4 + 'Type 1 common names and SYN must be located below a SP/AGG/HYBR taxon.\n'};
+if (f.matches('.e,.d,.s,.b,.n,.s2,.s3,.s4') && !(f.matches(':is(p.a,p.ae,p.c,.w)~p'))) {i++; log += '[#' + i + s4 + 'Type 1 common names, BAS and SYN must be located below a SP/AGG/HYBR taxon.\n'};
 if (f.matches('.e') && f.matches('p.d+p') || f.matches('.e2') && f.matches('p.d2+p')) {i++; log += '[#' + i + s4 + 'German common names should be placed after english common names.\n'};
-if (f.matches('.e,.d') && f.matches('p:is(.s,.s2,.s3,.s4,.u,.ue,.m,.v,.v2)+p') || f.matches('.e2,.d2') && f.matches('p.sh+p')) {i++; log += '[#' + i + s4 + 'SYN, SYN_H, SSP, SEG and PARENT should be placed after common names.\n'};
+if (f.matches('.e,.d') && f.matches('p:is(.b,.n,.s,.s2,.s3,.s4,.u,.ue,.m,.v,.v2)+p') || f.matches('.e2,.d2') && f.matches('p.sh+p')) {i++; log += '[#' + i + s4 + 'BAS, BAS_AUT, SYN, SYN_H, SSP, SEG and PARENT should be placed after common names.\n'};
+if (f.matches('.b,.n') && f.matches('p:is(.s,.s2,.s3,.s4)+p')) {i++; log += '[#' + i + s4 + 'Synonyms should be placed after basionyms.\n'};
 if (f.matches('.s,.s2,.s3,.s4') && f.matches('p:is(.u,.ue,.m,.v,.v2)+p')) {i++; log += '[#' + i + s4 + 'SSP, SEG and PARENT should be placed after synonyms.\n'};
-if (f.matches('p:is(.p,.p2)+p:is(.e,.e2,.d,.d2,[class^="s"],.u,.ue,.m,.v,.v2)')) {i++; log += '[#' + i + s4 + 'REF, REF_TITLE and REF_ID should be placed after common names, synonyms, SSP, SEG and PARENT.\n'};
+if (f.matches('p:is(.p,.p2)+p:is(.e,.e2,.d,.d2,.b,.n,[class^="s"],.u,.ue,.m,.v,.v2)')) {i++; log += '[#' + i + s4 + 'REF, REF_TITLE and REF_ID should be placed after common names, basionyms, synonyms, SSP, SEG and PARENT.\n'};
 if (f.matches('.v,.v2') && !(f.matches(':is(.w)~p'))) {i++; log += '[#' + i + s4 + 'PARENT taxons must be located below a HYBR taxon.\n'};
+if (f.matches('.n') && !(f.matches(':is(.b)+p')) || f.matches('.b') && !(/, \d\d\d\d<\/span>/.test(d))) {i++; log += '[#' + i + s4 + 'Every BAS name must be directly followed by a proper BAS_AUT.\n'};
 if (f.matches('.v2') && !(f.matches('p:is(.v)+p'))) {i++; log += '[#' + i + s4 + 'A hybrid PARENT taxon must be directly followed by the second PARENT.\n'};
 if (f.matches('.l') && !(f.matches('p:is(.r,.r2)+p')) || f.matches('.p,.p2') && !(f.matches('p.l+p'))) {i++; log += '[#' + i + s4 + 'Every REF must be followed by a proper REF_TITLE and REF_ID.\n'};
 
 // Data types
-if (f.matches('p:not(.x9,.x8,.x7,.x6,.x5,.x4,.x3,.x2,.x,.xs,.xp,.f,.y,.t,.j,.h,.g,.i,.o,.a,.ae,.c,.b,.k,.n,.m,.u,.ue,.w,.v,.v2,.e,.e2,.d,.d2,.s,.s2,.s3,.s4,.sh,.r,.r2,.l,.p,.p2)')) {i++; log += '[#' + i + s3 + 'Use a valid data type.\n'};
+if (f.matches('p:not(.x9,.x8,.x7,.x6,.x5,.x4,.x3,.x2,.x,.xs,.xp,.f,.y,.t,.j,.h,.g,.i,.o,.a,.ae,.c,.b,.n,.m,.u,.ue,.w,.v,.v2,.e,.e2,.d,.d2,.s,.s2,.s3,.s4,.sh,.r,.r2,.l,.p,.p2)')) {i++; log += '[#' + i + s3 + 'Use a valid data type.\n'};
 });
 
 
@@ -223,6 +226,7 @@ if (testArray[0] == 'SPGR') {i++; log += '[#' + i + s7 + 'GEN ' + f.innerHTML + 
 
 ////5 Check for duplicate common names and taxa
 var s8 = ']  Duplicate name detected for ';
+var s9 = ']  Duplicate basionym/synonym detected for ';
 
 // Create test arrays
 var testArrayE = [];
@@ -230,6 +234,7 @@ var testArrayE2 = [];
 var testArrayD = [];
 var testArrayD2 = [];
 var testArrayTX = [];
+var testArrayBS = [];
 
 // Collect all names
 $$('p[id]:not(.o),.e,.e2,.d,.d2').forEach(f=>{
@@ -238,7 +243,8 @@ if (f.matches('.e')) testArrayE.push(f.innerHTML);
 if (f.matches('.e2')) testArrayE2.push(f.innerHTML);
 if (f.matches('.d')) testArrayD.push(f.innerHTML);
 if (f.matches('.d2')) testArrayD2.push(f.innerHTML);
-if (f.matches('p[id]')) testArrayTX.push(f.id.replace(/_/g,' '))});
+if (f.matches('p[id]')) testArrayTX.push(f.id.replace(/_/g,' '));
+if (f.matches('.b,.s,.s2,.s3,.s4')) testArrayBS.push(f.id.replace(/_/g,' ').replace(/b\$/g,''))});
 
 // Sort the names
 testArrayE.sort();
@@ -246,6 +252,7 @@ testArrayE2.sort();
 testArrayD.sort();
 testArrayD2.sort();
 testArrayTX.sort();
+testArrayBS.sort();
 
 // Check the arrays for duplicates
 for (let t = 0; t < testArrayE.length - 1; t++) {
@@ -264,6 +271,11 @@ for (let t = 0; t < testArrayTX.length - 1; t++) {
 	if (testArrayTX[t].localeCompare(testArrayTX[t+1], 'en') == 0) {
 		testArrayTX[t] = testArrayTX[t].replace(/.+\$/,''); testArrayTX[t] = testArrayTX[t].charAt(0).toUpperCase() + testArrayTX[t].slice(1);
 		i++; log += '[#' + i + s8 + 'taxon "' + testArrayTX[t] + '". One of them must be considered invalid.\n'};
+};
+for (let t = 0; t < testArrayBS.length - 1; t++) {
+	if (testArrayBS[t].localeCompare(testArrayBS[t+1], 'en') == 0) {
+		testArrayBS[t] = testArrayBS[t].charAt(0).toUpperCase() + testArrayBS[t].slice(1);
+		i++; log += '[#' + i + s9 + 'taxon "' + testArrayBS[t] + '". One of them should be deleted.\n'};
 };
 
 
@@ -302,7 +314,7 @@ if (format == 'tsv') {var del = "\t"}; // Delimiter assignment
 
 	//Build dsv content
 	var file_cont = 'META_DATASET' + del + q + version + q + "\n" + 'META_ORIGIN' + del + q + 'https://lepitaxa.github.io' + q;
-	$$('p:not(.l),.ls').forEach(f=>{file_cont += '\n' + convert(f.classList) + del + q + f.innerHTML + q});
+	$$('p:not(.b,.l),.bs,.ls').forEach(f=>{file_cont += '\n' + convert(f.classList) + del + q + f.innerHTML + q});
 
 var file_link = document.createElement('a'); // Generate, click and remove download link
 file_link.setAttribute('download',version + '.' + format);
@@ -315,12 +327,14 @@ function xml() {
 	var file_cont = '<lepitaxa>\n<meta dataset="' + version + '" origin="https://lepitaxa.github.io' + '" />\n\n<taxa>\n';
 
 	//2 xml main, add taxalist
-	$$('p:not(.e,.e2,.d,.d2,[class^="s"],.u,.ue,.m,.v,.v2,.r,.r2,.l,.p,.p2)').forEach(f=>{
+	$$('p:not(.e,.e2,.d,.d2,.b,.n,[class^="s"],.u,.ue,.m,.v,.v2,.r,.r2,.l,.p,.p2)').forEach(f=>{
 		var sib = (f.matches('.h,.j,.t,.y,.f,.x,.x9')) ? f.nextElementSibling.firstElementChild : f.nextElementSibling;
-		var REF = ''; var RTI = ''; var RID = ''; var nEN = ''; var nDE = ''; var SYN = ''; var SUB = ''; var PAR = ''; // Start element lists
+		var REF = ''; var RTI = ''; var RID = ''; var nEN = ''; var nDE = ''; var BAS = ''; var AUT = ''; var SYN = ''; var SUB = ''; var PAR = ''; // Start element lists
 		while (sib) { // Loop for each taxon
 		if (sib.matches('.e,.e2')) nEN += '\t\t<com_name lang="en">' + sib.innerHTML + '</com_name>\n'; // If sib matches selector, add to nEN list
 		else if (sib.matches('.d,.d2')) nDE += '\t\t<com_name lang="de">' + sib.innerHTML + '</com_name>\n'; // If sib matches selector, add to nDE list
+		else if (sib.matches('.b')) BAS += '\t\t<bas>' + sib.firstElementChild.innerHTML + '</bas>\n'; // If sib matches selector, add to BAS list
+		else if (sib.matches('.n')) AUT += '\t\t<bas_aut>' + sib.innerHTML + '</bas_aut>\n'; // If sib matches selector, add to AUT list
 		else if (sib.matches('[class^="s"]')) SYN += '\t\t<syn>' + sib.innerHTML + '</syn>\n'; // If sib matches selector, add to SYN list
 		else if (sib.matches('.u,.ue,.m')) SUB += '\t\t<subtaxon type="' + convert(sib.classList) + '">' + sib.innerHTML + '</subtaxon>\n'; // If sib matches selector, add to SUB list
 		else if (sib.matches('.v,.v2')) PAR += '\t\t<parent>' + sib.innerHTML + '</parent>\n'; // If sib matches selector, add to PAR list
@@ -330,7 +344,7 @@ function xml() {
 		else break
 		sib = sib.nextElementSibling};
 
-		file_cont += '\t<taxon type="' + convert(f.classList) + '">\n\t\t<name>' + f.innerHTML + '</name>\n' + nEN + nDE + SYN + SUB + PAR + REF + RTI + RID + '\t</taxon>\n'});
+		file_cont += '\t<taxon type="' + convert(f.classList) + '">\n\t\t<name>' + f.innerHTML + '</name>\n' + nEN + nDE + BAS + AUT + SYN + SUB + PAR + REF + RTI + RID + '\t</taxon>\n'});
 
 	//3 xml end
 	file_cont += '</taxa>\n</lepitaxa>';
@@ -348,5 +362,5 @@ function convert(dt) {
 dt = dt.toString();
 dt = dt.replace(/(en|de|hide|open)/g,'');
 dt = dt.replace(/ /g,'').replace('x9','ORD').replace('x8','SUBORD').replace('x7','INFRAORD').replace('x6','PARVORD').replace('x5','MICROORD').replace('x4','SECT').replace('x3','SUBSECT').replace('x2','INFRASECT').replace('xs','SERIES').replace('xp','PARAPHYLUM').replace('ae','SP_EXT').replace('ue','SSP_EXT').replace('bs','BAS').replace('ls','REF_TITLE').replace('sh','SYN_H').replace('w','HYBR');
-dt = dt.replace(/\d/g,'').replace('x','SUPERFAM').replace('f','FAM').replace('y','SUBFAM').replace('t','TRI').replace('j','SUBTRI').replace('h','INFRATRI').replace('g','GEN').replace('i','SUBGEN').replace('o','SPGR').replace('a','SP').replace('c','AGG').replace('k','BAS_AUT').replace('n','BAS_GBIF').replace('m','SEG').replace('u','SSP').replace('v','PARENT').replace('r','REF').replace('l','REF_TITLE').replace('p','REF_ID').replace('e','NAME_EN').replace('d','NAME_DE').replace('s','SYN');
+dt = dt.replace(/\d/g,'').replace('x','SUPERFAM').replace('f','FAM').replace('y','SUBFAM').replace('t','TRI').replace('j','SUBTRI').replace('h','INFRATRI').replace('g','GEN').replace('i','SUBGEN').replace('o','SPGR').replace('a','SP').replace('c','AGG').replace('n','BAS_AUT').replace('m','SEG').replace('u','SSP').replace('v','PARENT').replace('r','REF').replace('l','REF_TITLE').replace('p','REF_ID').replace('e','NAME_EN').replace('d','NAME_DE').replace('s','SYN');
 return dt};
